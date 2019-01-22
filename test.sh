d558cbe4f62e2df512986e03e34bf05666d379b8
Wojciech Jabłoński <wj359634@students.mimuw.edu.pl>
2019-01-23 00:35:55 +0100

    added simple test, cleanup

--- test.sh
+++ test.sh
@@ -2,22 +2,22 @@
 
 echo "SHOULD FAIL:"
 
-for f in latte_tests/bad/*.lat; do echo $f && ./latc $f --silent; done
+for f in latte_tests/bad/*.lat; do echo $f && ./latc_llvm $f --silent; done
 
 echo "SHOULD NOT FAIL:"
 
 for f in latte_tests/good/*.lat
 do
-    echo $f && ./latc $f --silent --mrjp
+    echo $f && ./latc_llvm $f --silent --mrjp
 done
 
 # echo "PROBABLY SHOULD FAIL:"
 
-# for f in mrjp-tests/bad/**/*.lat; do echo $f && ./latc $f --silent; done
+# for f in mrjp-tests/bad/**/*.lat; do echo $f && ./latc_llvm $f --silent; done
 
 # echo "PROBABLY SHOULD NOT FAIL:"
 
 # for f in mrjp-tests/good/basic/*.lat
 # do
-#     echo $f && ./latc $f --silent --mrjp
+#     echo $f && ./latc_llvm $f --silent --mrjp
 # done
